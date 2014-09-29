2b863bcd21c0383c4a5f197243ecc960015f0492
Merili <meriliasu@gmail.com>
2014-09-29 20:24:20 +0300

    Initial commit
    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Funktsioonid ja nende defineerimine</title>
+</head>
+<body>
+	<h2>Funktsiooni defineerimine</h2>
+	<?php
+	?>
+</body>
+</html>
